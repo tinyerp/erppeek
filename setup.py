8ec147762569289ea21d4ef164ba703b6efd46b9
Florent Xicluna <florent.xicluna@gmail.com>
2012-08-27 07:28:18 +0200

    add the configuration for the tests

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import with_statement
+import sys
 
 from setuptools import setup
 
@@ -16,6 +17,12 @@ with open('README.rst') as f:
 
 # with open('CHANGES.rst') as f:
 #     readme += '\n\n\n' + f.read()
+if sys.version_info < (3,):
+    tests_require = ['mock', 'unittest2'],
+    test_suite = 'unittest2.collector'
+else:
+    tests_require = ['mock', 'unittest2py3k']
+    test_suite = 'unittest2.collector.collector'
 
 setup(
     name='ERPpeek',
@@ -46,4 +53,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
+    tests_require=tests_require,
+    test_suite=test_suite,
 )
