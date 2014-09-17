9ce681508a8f3c78f27d37560d3a7f689fcf9ff3
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2014-09-17 12:06:20 +0200

    Fix environments initialization for v8 new API

--- erppeek.py
+++ erppeek.py
@@ -250,7 +250,7 @@ def start_odoo_services(options=None, appname=None):
             odoo.service.start_internal()
         else:   # Odoo v8
             try:
-                odoo.Environment._local.environments = odoo.osv.env.WeakSet()
+                odoo.api.Environment._local.environments = odoo.api.Environments()
             except AttributeError:
                 pass
 
