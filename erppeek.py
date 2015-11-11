581fe15a04f1d2def3a7775f4a16a9d76a18c8d7
Edison Ibañez <arkhan.xxx@gmail.com>
2015-11-11 16:01:26 -0500

    start iptrhon if is installed

--- erppeek.py
+++ erppeek.py
@@ -1675,4 +1675,8 @@ def main(interact=_interact):
         return interact(global_vars) if interact else global_vars
 
 if __name__ == '__main__':
-    main()
+    try:
+        import IPython
+        IPython.start_ipython(user_ns=main(interact=False), argv=[])
+    except ImportError:
+        main()
